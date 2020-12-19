bf29f34c022fec8e8e4560d6b944c17d349944a2
Moisés Nelson Flores <nelsonflores17@gmail.com>
2020-12-18 22:20:41 -0600

    Signals para crear Perfil de Usuario

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+'''
+#import para los signals
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+'''
+
 # Create your models here.
 
 class Customer(models.Model):
@@ -50,3 +57,31 @@ class Order(models.Model):
 
 	def __str__(self):
 		return self.product.name
+
+'''
+#Perfil de usuario asociado: ejemplo
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+	first_name = models.CharField(max_lengh=200, null=True, blank=True)
+	last_name = models.CharField(max_length=200, null=True, blank=True)
+	phone = models.CharField(max_length=200, null=True, blank=True)
+
+	def __str__(self):
+		return self.first_name
+
+#forma 1: con decorator reciever
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)
+		print('Profile created!')
+
+#forma 2: 
+def update_profile(sender, instance, created, **kwargs):
+	if created == False:
+		instance.profile.save()
+		print('Profile updated')
+
+post_save.connect(update_profile, sender=User)
+
+'''
